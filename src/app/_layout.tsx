8468eb57b1db449e2f0e3cdf2f52c61fae57aaf3
Aren <keshishi.aren@gmail.com>
2026-09-05 19:18:02 -0700

    Updated linting/typescript configuration

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,36 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { Stack, Redirect, useSegments } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from 'react-native';
+import { useState } from 'react';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
 
 SplashScreen.preventAutoHideAsync();
 
-export default function TabLayout() {
+export default function RootLayout() {
+  const [currentUser, setCurrentUser] = useState<boolean>(false);
+
+  const segments = useSegments();
+  const inAuth = segments[0] === 'auth';
+
+  if (!currentUser && !inAuth) {
+    return <Redirect href="/auth/login" />;
+  }
+
+  if (currentUser && inAuth) {
+    return <Redirect href="/tabs/home" />;
+  }
+
   const colorScheme = useColorScheme();
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AnimatedSplashOverlay />
-      <AppTabs />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="auth" />
+        <Stack.Screen name="tabs" />
+      </Stack>
     </ThemeProvider>
   );
 }
